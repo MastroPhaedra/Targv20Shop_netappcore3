1f263dbcfdc5dcc5173fc46bb02a1151c99ee016
MastroPhaedra <zakharmakar1@gmail.com>
2021-12-25 21:53:25 +0200

    Добавьте файлы проекта.

---
+++ Targv20Shop.Data/Targv20ShopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Targv20Shop.Core.Domain;
+
+namespace Targv20Shop.Data
+{
+    public class Targv20ShopDbContext : DbContext
+    {
+        public Targv20ShopDbContext(DbContextOptions<Targv20ShopDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ExistingFilePath> ExistingFilePath { get; set; }
+    }
+}
